1094d1e6f5cf7a927b41f038d24c856658d59d61
helenavainmaa <helena.vainmaa@fooddocs.com>
2024-03-16 15:24:42 +0200

    iamges

--- theatre-box/image_controller.py
+++ theatre-box/image_controller.py
@@ -1,18 +1,22 @@
 import pygame
-carnival = "backgrounds/Carnival_1.png"
-pirateship = "backgrounds/pirateship1.png"
-mars = "backgrounds/mars.webp"
-tram = "backgrounds/tram.jpeg"
-haunted = "backgrounds/haunted.png"
 
 def open_image(scene_name):
-    if (scene_name == "90s Kopli tram"):
-        image = tram
     pygame.init()
     screen = pygame.display.set_mode((1200, 900))
+
+    image_paths = {
+        "90s Kopli tram": "backgrounds/tram.jpeg",
+        "Carnival": "backgrounds/Carnival_1.png",
+        "Pirate Ship": "backgrounds/pirateship1.png",
+        "Mars": "backgrounds/mars.webp",
+        "Haunted": "backgrounds/haunted.png"
+    }
+
+    image = image_paths.get(scene_name, "backgrounds/Carnival_1.png")  # Default to Carnival if not found
     background = pygame.image.load(image)
     screen.blit(background, (0, 0))
     pygame.display.flip()
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -20,15 +24,5 @@ def open_image(scene_name):
                 running = False
     pygame.quit()
 
-
-# def take_user_image():
-#     # take an image with the computer webcam
-#
-# def take_stage_image():
-#     # take an image with the external webcam
-#
-
 if __name__ == "__main__":
-    global image
-    image = carnival
     open_image("90s Kopli tram")
